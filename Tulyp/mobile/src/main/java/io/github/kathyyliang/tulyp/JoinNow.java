b37f6275d26e57f64184fc4aa51e475e307a7de1
Kathy Liang <kathyyliang@berkeley.edu>
2016-04-19 17:14:47 -0700

    progress on sign in and join now

---
+++ Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/JoinNow.java
@@ -0,0 +1,13 @@
+package io.github.kathyyliang.tulyp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JoinNow extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_now);
+    }
+}
