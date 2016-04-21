509630d8ae34c78916894d3d84dd482bd37730b5
Kathy Liang <kathyyliang@berkeley.edu>
2016-04-20 20:21:25 -0700

    card fragment on watch

--- Tulyp/wear/src/main/java/io/github/kathyyliang/tulyp/MainActivity.java
+++ Tulyp/wear/src/main/java/io/github/kathyyliang/tulyp/MainActivity.java
@@ -1,7 +1,10 @@
 package io.github.kathyyliang.tulyp;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.wearable.view.CardFragment;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
@@ -13,12 +16,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+
+        /*final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
-        });
+        });*/
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        CardFragment cardFragment = CardFragment.create("Tulyp",
+                "is running in the background.");
+        fragmentTransaction.add(R.id.frame_layout, cardFragment);
+        fragmentTransaction.commit();
     }
 }
