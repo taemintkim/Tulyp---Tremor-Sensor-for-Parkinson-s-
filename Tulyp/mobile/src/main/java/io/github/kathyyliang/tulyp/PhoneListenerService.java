887d94ee4119c400a91178d270d7adca1308ff14
MosNicholas <mosnicholas@berkeley.edu>
2016-04-20 22:23:30 -0700

    adding sensor data tracking

--- Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/PhoneListenerService.java
+++ Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/PhoneListenerService.java
@@ -1,64 +1,38 @@
 package io.github.kathyyliang.tulyp;
 
-import android.app.Service;
 import android.content.Intent;
-import android.os.IBinder;
+import android.util.Log;
 
+import com.google.android.gms.common.data.FreezableUtils;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 
-import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class PhoneListenerService extends WearableListenerService {
-    public void onMessageReceived(MessageEvent messageEvent) {
-        String person;
-        String location;
 
+    private final static String TREMOR_DATA_PATH = "/tremor_data_point";
+    private final static String TREMOR_DATA = "tremor_data";
 
-//        if( messageEvent.getPath().equalsIgnoreCase(DETAIL) ) {
-//
-//            String value = new String(messageEvent.getData(), StandardCharsets.UTF_8);
-//            Intent intent = new Intent(this, detail.class );
-//            String[] message = value.split(";");
-//
-//
-//            person = message[0];
-//            location = message[1];
-//            intent.putExtra("location", location);
-//            intent.putExtra("person", person);
-//            System.out.println("phone listener received " + location);
-//
-//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//
-//            //you need to add this flag since you're starting a new activity from a service
-//
-//
-//
-//
-//            startActivity(intent);
-//
-//
-//
-//        }else if( messageEvent.getPath().equalsIgnoreCase(SHAKE) ) {
-//
-//            String value = new String(messageEvent.getData(), StandardCharsets.UTF_8);
-//            showShake = new Intent(this, results.class);
-//            sendIntent = new Intent(this, PhoneToWatchService.class);
-//            sendIntent.putExtra("TYPE", "show");
-//
-//            showShake.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//            String[] coordinates = value.split(",");
-//            currLong = coordinates[0];
-//            currLat = coordinates[1];
-//
-//
-//
-//
-//
-//        }else {
-//            super.onMessageReceived( messageEvent );
-//        }
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {}
 
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        Log.d("T", "dataChanged called");
+        final List<DataEvent> events = FreezableUtils.freezeIterable(dataEvents);
+        for (DataEvent event : events) {
+            if (event.getType() == DataEvent.TYPE_CHANGED && event.getDataItem().getUri().getPath().equals(TREMOR_DATA_PATH)) {
+                DataMapItem dataMapItem = DataMapItem.fromDataItem(event.getDataItem());
+                DataMap repData = dataMapItem.getDataMap().getDataMap(TREMOR_DATA);
+
+                System.out.println(repData.toString());
+            }
+        }
     }
 
 }
