0172e28977cd007f7ac48e887ab1cf7886e14ada
Kathy Liang <kathyyliang@berkeley.edu>
2016-05-03 23:55:26 -0700

    forgot to add a file

---
+++ Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/ViewMedicalProfile.java
@@ -0,0 +1,13 @@
+package io.github.kathyyliang.tulyp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewMedicalProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_medical_profile);
+    }
+}
