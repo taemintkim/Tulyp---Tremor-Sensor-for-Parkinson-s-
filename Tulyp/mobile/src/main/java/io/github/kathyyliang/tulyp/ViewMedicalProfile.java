7656b4bee306d356548677142eb49eafb59c5a21
Kathy Liang <kathyyliang@berkeley.edu>
2016-05-04 12:17:05 -0700

    toolbargit add Tulyp/

--- Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/ViewMedicalProfile.java
+++ Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/ViewMedicalProfile.java
@@ -2,6 +2,7 @@ package io.github.kathyyliang.tulyp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 
@@ -12,5 +13,9 @@ public class ViewMedicalProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_medical_profile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Medical Profile");
+        setSupportActionBar(toolbar);
     }
 }
