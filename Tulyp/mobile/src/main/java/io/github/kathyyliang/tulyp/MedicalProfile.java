f2ae8b30156d6cec2259a07a1e9ba93128fc3789
tae min kim <taemintkim@berkeley.edu>
2016-05-04 11:24:18 -0700

    forgot password working

--- Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/MedicalProfile.java
+++ Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/MedicalProfile.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 //todo: this whole activity. User profile data is in mUser.
 //todo: what if the patient wants to edit their info? Make an option to edit.
@@ -22,6 +25,15 @@ public class MedicalProfile extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Medical Profile");
         setSupportActionBar(toolbar);
+
+        EditText fullName = (EditText) findViewById(R.id.fullname);
+        EditText dateOfBirth = (EditText) findViewById(R.id.dateofbirth);
+        EditText feet = (EditText) findViewById(R.id.feet);
+        EditText inches = (EditText) findViewById(R.id.inches);
+        EditText pounds = (EditText) findViewById(R.id.pounds);
+        ArrayList<String> height = new ArrayList<String>();
+        height.add(feet.getText().toString());
+        height.add(inches.getText().toString());
     }
 
 
