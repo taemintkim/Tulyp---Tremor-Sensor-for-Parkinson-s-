2d00c3e5cfec37a46b38292a0b1ae550ff73d403
MosNicholas <mosnicholas@berkeley.edu>
2016-05-04 12:14:24 -0700

    updated doctor medication activity

--- Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/DoctorMedication.java
+++ Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/DoctorMedication.java
@@ -1,14 +1,18 @@
 package io.github.kathyyliang.tulyp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.HashMap;
 
 public class DoctorMedication extends AppCompatActivity {
     User user = TulypApplication.mUser;
-    HashMap<String, Drug> drugs = user.getMedications();
+    HashMap<String, Drug> drugs = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,27 @@ public class DoctorMedication extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Medication");
         setSupportActionBar(toolbar);
+    }
+
+    public void updateMedicationView(View view) {
+        EditText medication = (EditText) findViewById(R.id.editMedication);
+        EditText instructions = (EditText) findViewById(R.id.editDosage);
+        EditText warnings = (EditText) findViewById(R.id.editWarnings);
+        EditText start_date = (EditText) findViewById(R.id.editFirstDay);
+        EditText end_date = (EditText) findViewById(R.id.editLastDay);
+
+        String medName = medication.getText().toString();
+        String instr = instructions.getText().toString();
+        String warns = warnings.getText().toString();
+        String start = start_date.getText().toString();
+        String end = end_date.getText().toString();
+
+        Drug d = new Drug(instr, warns, start, end);
+
+        drugs.put(medName, d);
+        user.setMedications(drugs);
+
+        Intent intent = new Intent(getBaseContext(), ViewMedicalProfile.class);
+        startActivity(intent);
     }
 }
