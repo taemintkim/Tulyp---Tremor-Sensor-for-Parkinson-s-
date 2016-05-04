05bdb09f16e30a36d42fd57d69628636573e249d
tae min kim <taemintkim@berkeley.edu>
2016-05-04 12:04:18 -0700

    bunch of activities. med profile. account prof.

--- Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/AccountInfo.java
+++ Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/AccountInfo.java
@@ -1,18 +1,66 @@
 package io.github.kathyyliang.tulyp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class AccountInfo extends AppCompatActivity {
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_info);
+        context = this;
+        TextView email = (TextView) findViewById(R.id.email);
+        if (TulypApplication.mUser != null && TulypApplication.mUser.getEmail() != null) {
+            email.setText(TulypApplication.mUser.getEmail());
+        }
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Account Info");
         setSupportActionBar(toolbar);
+
+        Button button = (Button) findViewById(R.id.savebutton1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText oldp = (EditText) findViewById(R.id.currentpassword);
+                EditText newp = (EditText) findViewById(R.id.newpassword);
+                EditText newp2 = (EditText) findViewById(R.id.confirmnewpassword);
+                changepassword(TulypApplication.mUser.getEmail(), newp.getText().toString(), newp2.getText().toString());
+            }
+        });
+    }
+
+    private void changepassword(String email, String oldpw, String newpw) {
+        TulypApplication.mFirebase.getFirebaseRef().changePassword(email, oldpw, newpw, new Firebase.ResultHandler() {
+            @Override
+            public void onSuccess() {
+                // password changed
+                CharSequence text = "Password Changed!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration); //not sure if this way of passing in context is correct.
+                toast.show();
+            }
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                // error encountered
+                CharSequence text = "Error: Try again.";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration); //not sure if this way of passing in context is correct.
+                toast.show();
+            }
+        });
     }
 }
--- Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/MedicalProfile.java
+++ Tulyp/mobile/src/main/java/io/github/kathyyliang/tulyp/MedicalProfile.java
@@ -1,39 +1,82 @@
 package io.github.kathyyliang.tulyp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-//todo: this whole activity. User profile data is in mUser.
-//todo: what if the patient wants to edit their info? Make an option to edit.
 public class MedicalProfile extends AppCompatActivity {
     User user = TulypApplication.mUser;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_profile);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("Medical Profile");
-        setSupportActionBar(toolbar);
-
+        context = this;
         EditText fullName = (EditText) findViewById(R.id.fullname);
         EditText dateOfBirth = (EditText) findViewById(R.id.dateofbirth);
         EditText feet = (EditText) findViewById(R.id.feet);
         EditText inches = (EditText) findViewById(R.id.inches);
         EditText pounds = (EditText) findViewById(R.id.pounds);
-        ArrayList<String> height = new ArrayList<String>();
-        height.add(feet.getText().toString());
-        height.add(inches.getText().toString());
+        if (user.getName() != null) {
+            fullName.setText(user.getName());
+        }
+        if (user.getBirthdate()!= null) {
+            dateOfBirth.setText(user.getBirthdate());
+        }
+        if (user.getHeight() != null) {
+            feet.setText(user.getHeight().get(0));
+        }
+        if (user.getHeight() != null) {
+
+            inches.setText(user.getHeight().get(1));
+        }
+        if (user.getWeight() != null) {
+
+            pounds.setText(user.getWeight());
+        }
+            Spinner spinner = (Spinner) findViewById(R.id.spinner);
+            spinner.setSelection(user.getGender());
+
+        Button button = (Button) findViewById(R.id.savebutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText fullName = (EditText) findViewById(R.id.fullname);
+                EditText dateOfBirth = (EditText) findViewById(R.id.dateofbirth);
+                EditText feet = (EditText) findViewById(R.id.feet);
+                EditText inches = (EditText) findViewById(R.id.inches);
+                EditText pounds = (EditText) findViewById(R.id.pounds);
+                ArrayList<String> height = new ArrayList<String>();
+                height.add(feet.getText().toString());
+                height.add(inches.getText().toString());
+                // spinner entries: 0 - male, 1 - female, 2 - other
+                Spinner spinner = (Spinner) findViewById(R.id.spinner);
+                int gender = spinner.getSelectedItemPosition();
+                user.setWeight(pounds.getText().toString());
+                user.setHeight(height);
+                user.setBirthdate(dateOfBirth.getText().toString());
+                user.setName(fullName.getText().toString());
+                user.setGender(gender);
+                TulypApplication.mFirebase.setNewUserInfo(user);
+                CharSequence text = "Information Updated!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration); //not sure if this way of passing in context is correct.
+                toast.show();
+            }
+        });
     }
 
 
